cd69fa64a4366037edb2137ca32e1f6092d1d317
dsrc338 <dsrc338@gmail.com>
2026-03-29 12:33:45 -0300

    add dockerfile

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http  = require('http');
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
 	res.end('Hello David DevOps Journey.');
 
 });
 
-	server.listen(3000, () =>{
-	console.log('Server running on port 3000');
+	server.listen(PORT, () =>{
+	console.log('Server running on port ${PORT}');
 });
